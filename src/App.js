65788c5ef32fc50da9bf0018f00d9364fb27f216
Govindasaikakaraparthi <govindasaikakaraparthi@gmail.com>
2023-10-12 10:54:55 +0530

    tester

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+import CompA from './components/CompA';
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+function App() {
+  const details=[
+    {
+      name:"ammu",
+      roll:"636"
+    },
+    {
+      name:"jhanu",
+      roll:"668" 
+    }
+  ]
+  return (
+    <>
+    <BrowserRouter>
+    <ul>
+      <li>
+      <Link to="/CompA">click to CompA</Link>
+      </li>
+    </ul>
+      
+      <Routes>
+        <Route path="/CompA" element={<CompA data={details}/>}></Route>
+        
+    </Routes>
+    
+    
+    
+    
+    </BrowserRouter>
+     
+    
+    
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/components/CompA.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function CompA(props) {
+  return (
+    <>
+    <table border={1}>
+        <tr>
+            <td>Name</td>
+            <td>RollNo</td>
+        </tr>
+  
+    {
+        props.data.map((e,i)=>{
+            return(
+                <>
+                <tr>
+                  <td><h1>{e.name}</h1></td>
+                  <td><h1>{e.roll}</h1></td>
+                  </tr>
+                </>
+            )
+        })
+    }
+    
+    </table>
+
+    </>
+  )
+}
